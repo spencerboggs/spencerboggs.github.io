b00d283bff5199f648f8385f677a6efc9f0ad52a
Spencer Boggs <spencerdboggs@gmail.com>
2023-06-20 16:46:42 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+// switch to phonestyle.css on load if mobile
+if (window.innerWidth < 600) {
+    document.getElementById("style").setAttribute("href", "phonestyle.css");
+}
 
-
-let details = navigator.userAgent;
-let regexp = /android|iphone|kindle|ipad/i;
-let isMobileDevice = regexp.test(details);
-const styleLink = document.getElementById("style");
-
-if (isMobileDevice) {
-  if (document.URL.includes("/")) {
-    styleLink.setAttribute("href", "../phonestyle.css");
-  } else {
-    styleLink.setAttribute("href", "phonestyle.css");
-  }
+window.onload = function () {
+    setTimeout(function () {
+        window.scrollTo(0, 0);
+    }, 100);
+    
+    setTimeout(function () {
+        // then scroll to the #hash element
+        if (window.location.hash) {
+            let hash = window.location.hash;
+            window.location.hash = '';
+            window.location.hash = hash;
+        }
+    }, 5000);
 }
