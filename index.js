bf5ee3526f8f7d0110bcae89ebd4c711ca765457
Spinny2005 <spencer_boggs@icloud.com>
2023-06-26 15:07:01 -0700

    Fixed js file to work with new html files

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // switch to phonestyle.css on load if mobile
 if (window.innerWidth < 600) {
-    document.getElementById("style").setAttribute("href", "phonestyle.css");
-} else {
-    document.getElementById("style").setAttribute("href", "style.css");
+    //document.getElementById("style").setAttribute("href", "phonestyle.css");\
+    //rather than setting the file to phonestyle.css just replace style.css with phonestyle.css so that ../ works
+    // Do this by replacing the last 9 characters of the string
+    let style = document.getElementById("style").getAttribute("href");
+    style = style.substring(0, style.length - 9) + "phonestyle.css";
+    document.getElementById("style").setAttribute("href", style);
+
 }
 
 window.onload = function () {
