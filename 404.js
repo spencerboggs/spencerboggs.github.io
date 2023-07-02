fe690418ec9dc09f836f546df8a9ee99afd7a6a2
Spencer Boggs <spencerdboggs@gmail.com>
2023-07-02 00:50:39 -0700

    Add files via upload

--- 404.js
+++ 404.js
@@ -1,8 +1,3 @@
-// If the user requests a page that doesn't exist, this is the page that will be shown.
-// But try adding .html to the end of the URL and see what happens.
-// If the page still doesn't exist, then this page will be shown.
-// If the page does exist, then the page will be shown.
-
 window.onload = function () {
     if (!window.location.pathname.endsWith('.html')) {
         var newUrl = window.location.pathname + '.html';
